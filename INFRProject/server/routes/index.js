5b3d4b50f97371c3c7d749e8253708d853eed9aa
bennett <bennett.elmhurst@ontariotechu.net>
2024-11-26 20:43:53 -0500

    things

--- INFRProject/server/routes/index.js
+++ INFRProject/server/routes/index.js
@@ -5,25 +5,40 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Home' });
 });
+
 /* GET home page. */
 router.get('/home', function(req, res, next) {
   res.render('index', { title: 'Home' });
 });
+
 /* GET About page. */
 router.get('/aboutus', function(req, res, next) {
   res.render('index', { title: 'About us' });
 });
+
 /* GET products page. */
 router.get('/products', function(req, res, next) {
   res.render('index', { title: 'Products' });
 });
+
 /* GET service page. */
 router.get('/service', function(req, res, next) {
   res.render('index', { title: 'Service' });
 });
+
 /* GET contactus page. */
 router.get('/contactus', function(req, res, next) {
   res.render('index', { title: 'Contact Us' });
+});
+
+/* GET login page. */
+router.get('/login', function(req, res, next) {
+  res.render('login', { title: 'Login' });
+});
+
+/* GET register page. */
+router.get('/register', function(req, res, next) {
+  res.render('register', { title: 'Register' });
 });
 
 module.exports = router;
